54265754cacea8e01bbf8e9ac75294e076088436
denizbabayaka <56898154+denizbabayaka@users.noreply.github.com>
2021-04-01 17:46:45 -0400

    login page

--- python/FLASK-O4-HANDLING-FORMS-RUN-ON-ec2-linux2/app-form-handling.py
+++ python/FLASK-O4-HANDLING-FORMS-RUN-ON-ec2-linux2/app-form-handling.py
@@ -10,21 +10,32 @@ def home():
 def greet():
     if 'user' in request.args:
         usr = request.args['user']
-        return render_template('greet.html', user = usr)
+        return render_template('greet.html', user = usr.title())
     else:
         return render_template('greet.html', user = 'Send your user name with  user param in query string ') 
           
 
+@app.route('/login', methods = ["GET","POST"])
+def login():
+    if request.method == "POST":
+        user_name = request.form["username"]
+        password = request.form["password"]
+        if password == 'clarusway':
+            return render_template('secure.html', user = user_name.title())
+        else: 
+            return render_template('login.html', user=user_name.title(), control = True )           
+
+    else:
+        return render_template('login.html', control = False)
 
 
 
 
 
 
-
-
-
+#Add a statement to run the Flask application which can be reached from any host on port 80
 
 
 if __name__ == '__main__':
-    app.run(debug =True)    
+    #app.run(debug =True)   
+    app.run(host='0.0.0.0', port=80) 
