39851ed9e47e09d53b3c996e3fc5c7f6dc3bb14e
denizbabayaka <56898154+denizbabayaka@users.noreply.github.com>
2021-04-01 16:46:17 -0400

    flask get post method

---
+++ python/FLASK-O4-HANDLING-FORMS-RUN-ON-ec2-linux2/app-form-handling.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', name = 'Deniz')
+
+@app.route('/greet', methods = ["GET"])
+def greet():
+    if 'user' in request.args:
+        usr = request.args['user']
+        return render_template('greet.html', user = usr)
+    else:
+        return render_template('greet.html', user = 'Send your user name with  user param in query string ') 
+          
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug =True)    
