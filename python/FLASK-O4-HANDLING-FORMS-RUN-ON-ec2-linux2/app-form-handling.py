cd0d2f36e4549a87c0a1996b75be883fb21d137c
denizbabayaka <56898154+denizbabayaka@users.noreply.github.com>
2021-04-04 17:26:16 -0400

    projects file

--- python/FLASK-O4-HANDLING-FORMS-RUN-ON-ec2-linux2/app-form-handling.py
+++ python/FLASK-O4-HANDLING-FORMS-RUN-ON-ec2-linux2/app-form-handling.py
@@ -37,5 +37,5 @@ def login():
 
 
 if __name__ == '__main__':
-    #app.run(debug =True)   
-    app.run(host='0.0.0.0', port=80) 
+    app.run(debug =True)   
+    #app.run(host='0.0.0.0', port=80) 
